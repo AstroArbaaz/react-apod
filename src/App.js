feaa6e1a078ce2bdc3823f0870298db8080446ac
Arbaaz khan <mrkhan4231@gmail.com>
2020-07-03 19:13:40 +0530

    error 404 issue solved

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Route path='/' exact component={Home} />
         <Route path="/nasa_photo" component={NasaPhoto} />
